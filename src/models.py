48e443348618dcee1142b65995d5a9c7b084fcca
JonathanM1206 <jonathanmoesses@gmail.com>
2025-01-25 00:49:38 +0000

    sql Jona

--- src/models.py
+++ src/models.py
@@ -7,31 +7,60 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(String(15), primary_key=True) 
+    email = Column(String(25), nullable=False) 
+    password = Column(String(30), nullable=False) 
+    posts = relationship("Post", back_populates="autor") 
+    likes = relationship("Likes", back_populates="user")  
+    def to_dict(self):
+        return {}
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(String(15), primary_key=True) 
+    user_id = Column(String(15), ForeignKey('user.id')) 
+    content = Column(String(250), nullable=True) 
+    autor = relationship("User ", back_populates="posts") 
+    likes = relationship("Likes", back_populates="post") 
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+class Likes(Base): 
+    __tablename__ = 'likes' 
+    id = Column(Integer, primary_key=True) 
+    user_id = Column(String(15), ForeignKey('user.id'))  
+    post_id = Column(String(15), ForeignKey('post.id'))  
+    user = relationship("User ", back_populates="likes") 
+    post = relationship("Post", back_populates="likes") 
+
+    def to_dict(self):
+        return {}
+
+class Follower(Base): 
+    __tablename__ = 'follower'  
+    id = Column(Integer, primary_key=True) 
+    follower_id = Column(String(15), ForeignKey('user.id'))  
+    user_id = Column(String(15), ForeignKey('user.id'))  
+
+    def to_dict(self):
+        return {}
+
+class Following(Base): 
+    __tablename__ = 'following' 
+    id = Column(Integer, primary_key=True) 
+    follower_id = Column(String(15), ForeignKey('user.id')) 
+    user_id = Column(String(15), ForeignKey('user.id'))  
+
+    def to_dict(self):
+        return {}
+
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
